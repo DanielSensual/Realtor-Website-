77e41c5f7204999a0df626f15ac8d750b7ff4c61
DanielSensual <danielcastillo@mediageekz.com>
2026-02-04 22:22:57 -0500

    fix: restore Streamable video embed for hero banner

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("915cd2531c623e94")
+/******/ 		__webpack_require__.h = () => ("cc7c920c669c22cd")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
